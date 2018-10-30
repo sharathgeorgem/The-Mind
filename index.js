6402b52698c5aea5d6ee39f4a17b8c2e7cec9fea
Sharath George M <sharathgeorgem@gmail.com>
2018-10-30 10:16:21 +0530

    Added server.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-var app = require('express')
+var express = require('express')
+var path = require('path')
+var theMind = require('./theMind')
+
+var app = express()
+
 var server = require('http').createServer(app)
 
 server.listen(8080)
 
 var io = require('socket.io').listen(server)
 
+app.use(express.logger('dev'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+io.sockets.on('connection', function (socket) {
+  console.log('Client connected')
+  theMind.startGame(io, socket)
+})
